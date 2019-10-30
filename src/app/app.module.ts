4a0d2ce32f38e40fc3c67cd2527a5edff4355012
Luis <luis.cereno.t@gmail.com>
2019-10-30 19:43:47 -0300

    Se crea modulos de shared y pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,35 +4,39 @@ import { NgModule } from '@angular/core';
 //Rutas
 import { APP_ROUTES } from './app.routes';
 
+//Modulos 
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+// import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { ProgressComponent } from './pages/progress/progress.component';
+// import { HeaderComponent } from './shared/header/header.component';
+// import { SidebarComponent } from './shared/sidebar/sidebar.component';
+// import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+// import { Graficas1Component } from './pages/graficas1/graficas1.component';
+// import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    Graficas1Component,
-    PagesComponent,
     RegisterComponent
+    // NopagefoundComponent
+    // DashboardComponent,
+    // ProgressComponent,
+    // Graficas1Component,
+    // PagesComponent,
+    // HeaderComponent,
+    // SidebarComponent,
+    // BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
